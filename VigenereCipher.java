11bc8fb4fb6fb2fd6d609d935e0fa93dabeca98f
Ayz <120428408+ayz-x@users.noreply.github.com>
2025-11-24 14:02:25 -0800

    Update VigenereCipher.java

--- VigenereCipher.java
+++ VigenereCipher.java
@@ -5,13 +5,15 @@ public class VigenereCipher {
         System.out.println(test);
         test = decode(test, "sigma");
         System.out.println(test);
+
     }
     //precondition: keyword is all lowercase and all characters
     public static String encode(String plainText, String keyword){
         String code = ""; //string to be coded
+        int i2 = 0;
         for (int i = 0; i < plainText.length(); i++) {
             char uncodedChar = plainText.charAt(i);
-            int key = keyword.charAt(i%keyword.length()) - 'a';
+            int key = keyword.charAt(i2%keyword.length()) - 'a';
             if(Character.isLetter(uncodedChar)){
                 //if it is a character, code before adding
                 char codedChar;
@@ -23,6 +25,7 @@ public class VigenereCipher {
                 }
                 codedChar = (char)((uncodedChar-base+key)%26 + base); //code the char
                 code = code + codedChar; //add new coded char
+                i2++;
             }else{
                 code = code + uncodedChar; //add uncoded char
             }
@@ -32,8 +35,9 @@ public class VigenereCipher {
     //precondition: keyword is all lowercase and all characters
     public static String decode(String cipherText, String keyword){
         String decoded = ""; //string to be decoded
+        int i2 = 0;
         for (int i = 0; i < cipherText.length(); i++) {
-            int key = keyword.charAt(i%keyword.length()) -'a';
+            int key = keyword.charAt(i2%keyword.length()) -'a';
             char codedChar = cipherText.charAt(i);
             if(Character.isLetter(codedChar)){
                 //if it is a character, decode before adding
@@ -46,6 +50,7 @@ public class VigenereCipher {
                 }
                 decodedChar = (char)((codedChar-base-key + 26)%26 + base); //decode the char
                 decoded = decoded + decodedChar; //add new decoded char
+                i2++;
             }else{
                 decoded = decoded + codedChar; //not a character, just add
             }
